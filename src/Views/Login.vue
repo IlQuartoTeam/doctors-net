83100c14ac3741d05afa42b9814ff9ef59e157ba
filecc <fil.gia@live.it>
2023-06-29 19:06:45 +0200

    input component and login page (mobile)

--- src/Views/Login.vue
+++ src/Views/Login.vue
@@ -1,15 +1,36 @@
 <template>
-    <div>
-        
-    </div>
+    <section class="bg-doc-white p-4">
+        <div class="w-100">
+            <img class="d-block mx-auto img-fluid" src="/img/logo/hearts-no-track.svg" alt="logo">
+            <h1 class="text-uppercase text-doc-accent text-center mt-2">doctors<span class="text-doc-primary">net</span></h1>
+            <h2 class="text-center text-doc-blue fw-bold">Login</h2>
+            <InputComponent id="email_login" label="E-mail" type="e-mail" placeholder="gastanifrinzi@gmail.com" />
+            <InputComponent id="password_login" label="Password" type="password" placeholder="Password" />
+        </div>
+    </section>
 </template>
 
 <script>
+import InputComponent from '../components/InputComponent.vue';
     export default {
-        
-    }
+    components: { InputComponent }
+}
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/variables' as *;
+section{
+    width: 100%;
+    height: 100dvh;
+    display: grid;
+    place-items: center;
+}
 
+h1{
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+img{
+    max-width: 200px;
+}
 </style>
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <label :for="id" class="text-doc-blue mb-2 fs-6 fw-medium">{{ label }}</label>
+    <input :id="id" class="w-100 mb-3" :type="type" :placeholder="placeholder">
+</template>
+
+<script>
+export default {
+props: ['id', 'label', 'type', 'placeholder']
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/variables' as *;
+
+input {
+   
+    border: 1px solid $doc-primary;
+    border-radius: 5px;
+    padding: 1rem 20px;
+    display: block;
+
+    &::placeholder{
+        color: $doc-blue;
+        opacity: 60%;
+    }
+    &:focus-visible{
+        outline: 2px solid $doc-blue;
+    }
+
+}
+</style>
