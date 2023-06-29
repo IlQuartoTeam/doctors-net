904bb8a5c64336138089ee0da0594b52bbb3ae27
nicolozibra1 <nico.zibra@gmail.com>
2023-06-29 18:58:40 +0200

    master.scss

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <MapComponent cap="94017"/>
+        <NavbarComponent />
+        <!-- <MapComponent cap="94017"/>
         HOME
-        <ButtonComponent title="ciao" className="primary" />
+        <ButtonComponent title="ciao" className="primary" /> -->
     </div>
 </template>
 
 <script>
+import NavbarComponent from '../components/NavbarComponent.vue';
 import ButtonComponent from '../components/ButtonComponent.vue';
 import MapComponent from '../components/MapComponent.vue';
     export default {
-    components: { ButtonComponent, MapComponent }
+    components: { ButtonComponent, MapComponent, NavbarComponent }
 }
 </script>
 
