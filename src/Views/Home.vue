0646d8f5103fd6366e002f29eb7f059c1f05bcb6
filecc <fil.gia@live.it>
2023-06-30 09:45:25 +0200

    modified home

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -1,15 +1,14 @@
 <template>
     <div>
-       
+        
     </div>
 </template>
 
 <script>
-import NavbarComponent from '../components/NavbarComponent.vue';
-import ButtonComponent from '../components/ButtonComponent.vue';
-import MapComponent from '../components/MapComponent.vue';
+
+
     export default {
-    components: { ButtonComponent, MapComponent, NavbarComponent }
+   
 }
 </script>
 
