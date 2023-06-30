95aeb000391ff3e6a6efcbebac7b7c628c330753
Lip-7 <zhighofficial@gmail.com>
2023-06-30 09:32:55 +0200

    working in progress on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
     <footer class="bg-doc-primary">
-        <div class="iconAndCall d-flex justify-content-center gap-4 align-items-center flex-md-row flex-column w-100 flex-wrap justify-content-md-between">
+        <div
+            class="iconAndCall d-flex justify-content-center gap-4 align-items-center flex-md-row flex-column w-100 flex-wrap justify-content-md-between">
             <div class="icon d-flex align-items-center gap-2">
                 <div class="logo">
                     <img class="img-fluid" src="/img/logo/hearts.svg" alt="doctorsnet logo">
@@ -9,22 +10,24 @@
                     <h5 class="fw-bold text-white mb-0">Doctors net</h5>
                 </div>
             </div>
-            <div class="doctorButton">
-                <ButtonComponent title="Sei un medico? Iscriviti gratis" className="outline"  />
+            <div class="doctorButton d-flex">
+
+                <ButtonComponent title="Sei un medico? Iscriviti gratis" className="outline" />
             </div>
         </div>
-        <div class="linksAndPayment mt-4 gap-4 d-flex justify-content-center align-items-center flex-md-row flex-column w-100 flex-wrap justify-content-md-between">
+        <div
+            class="linksAndPayment mt-4 gap-4 d-flex justify-content-center align-items-center flex-md-row flex-column w-100 flex-wrap justify-content-md-between">
             <div class="links">
                 <div class="linksrow d-flex gap-4 my-1">
-                    <a class="text-white" href="#">I nostri specialisti</a>
-                    <a class="text-white" href="#">Assistenza</a>
+                    <RouterLink class="routeLink" :to="{name: ''}">I nostri specialisti</RouterLink>
+                    <RouterLink class="routeLink" :to="{name: ''}">Assistenza</RouterLink>
                 </div>
                 <div class="linksrow d-flex gap-4 my-1">
-                    <a class="text-white" href="#">Termini e Condizioni</a>
-                    <a class="text-white" href="#">Chi siamo</a>
+                    <RouterLink class="routeLink" :to="{name: ''}">Termini e Condizioni</RouterLink>
+                    <RouterLink class="routeLink" :to="{name: ''}">Chi siamo</RouterLink>
                 </div>
                 <div class="linksrow d-flex gap-4 my-1">
-                    <a class="text-white" href="#">Privacy Policy</a>
+                    <RouterLink class="routeLink" :to="{name: ''}">Privacy Policy</RouterLink>
                 </div>
             </div>
             <div class="paymentButton">
@@ -33,15 +36,26 @@
                 </a>
             </div>
         </div>
-        <div class="socials d-flex justify-content-center justify-content-md-end">
-            <div class="icons d-flex align-items-center">
-
+        <div class="socials my-3 d-flex justify-content-center justify-content-md-end">
+            <div class="icons d-flex align-items-center gap-2">
+                <a href="#"><IconBrandFacebook :size="30"/></a>
+                <a href="#"><IconBrandInstagram :size="30"/></a>
+                <a href="#"><IconBrandTwitter :size="30"/></a>
+                <a href="#"><IconBrandLinkedin :size="30"/></a>
             </div>
+        </div>
+        <div class="copyright">
+            <p>filecc - lip07 - raffaelecapaldo - nicolozibra1 - gaetano26</p>
         </div>
     </footer>
 </template>
   
 <script>
+import { IconBrandTwitter } from '@tabler/icons-vue';
+import { IconBrandFacebook } from '@tabler/icons-vue';
+import { IconBrandInstagram } from '@tabler/icons-vue';
+import { IconBrandLinkedin } from '@tabler/icons-vue';
+import { IconUser } from '@tabler/icons-vue';
 import ButtonComponent from './ButtonComponent.vue';
 export default {
     name: 'FooterComponent',
@@ -54,7 +68,12 @@ export default {
 
     },
     components: {
-    ButtonComponent
+    ButtonComponent,
+    IconUser,
+    IconBrandFacebook,
+    IconBrandInstagram,
+    IconBrandLinkedin,
+    IconBrandTwitter,
 },
     mounted() {
 
@@ -63,13 +82,24 @@ export default {
 </script>
   
 <style lang="scss" scoped>
-footer{
+@use '../assets/styles/variables' as *;
+footer {
     padding: 30px 50px;
-    .logo{
+
+    .logo {
         width: 50px;
     }
-    .linksrow{
-       font-size: 18px;
+
+    .linksrow {
+        font-size: 18px;
+        .routeLink{
+            color: $doc-white;
+        }
+    }
+    .socials{
+        a{
+            color: $doc-white;
+        }
     }
 }
 
