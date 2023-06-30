c34533374e36fdff206c9893b898cb5d31ef9d31
filecc <fil.gia@live.it>
2023-06-30 09:43:21 +0200

    added layout, removed Navbar from HOME

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
+
+
+    <template v-if="this.$route.path != '/login' && this.$route.path != '/register'">
+      <navbar-component />
+    </template>
     <router-view></router-view>
 </template>
 
 <script>
-  export default {
+import NavbarComponent from './components/NavbarComponent.vue';
 
-  }
+  export default {
+    mounted() {
+        console.log(this.$route.path);
+    },
+    components: { NavbarComponent }
+}
 </script>
 
 <style lang="scss" scoped>
