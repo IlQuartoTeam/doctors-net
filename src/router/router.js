e9d358c19d540d5d45720a889fffcaab73ba4505
filecc <fil.gia@live.it>
2023-06-30 15:37:32 +0200

    logout route

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,14 @@ import Home from '../Views/Home.vue'
 import Login from '../Views/Login.vue'
 import Register from '../Views/Register.vue'
 import User from '../Views/User.vue'
+import Logout from '../Views/Logout.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/login',name: 'login', component: Login },
     { path: '/register', component: Register },
-    { path: '/users/profile', component: User}
+    { path: '/users/profile', component: User},
+    {path: '/logout', component: Logout}
   ]
   
 
