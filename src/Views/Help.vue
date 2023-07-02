68f11036b7190106218f332153c5661c1b1bf719
Lip-7 <zhighofficial@gmail.com>
2023-07-02 21:51:58 +0200

    fixed navbar and footer links

--- src/Views/Help.vue
+++ src/Views/Help.vue
@@ -1,16 +1,35 @@
 <template>
-    <div>
-       <PrivacyComponent />
+    <div ref="privacySection">
+        <PrivacyComponent />
     </div>
 </template>
 
 <script>
 import PrivacyComponent from '../components/PrivacyComponent.vue';
-    export default {
-        components: {PrivacyComponent }
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: { PrivacyComponent },
+    methods: {
+        scrollToSection() {
+            console.log('ciao');
+            const hash = window.location.hash;
+            if (hash) {
+                const element = this.$refs[hash.substring(1)];
+                if (element) {
+                    element.scrollIntoView({ behavior: 'smooth' });
+                }
+            }
+        },
+    },
+    mounted() {
+        if (window.location.hash) this.scrollToSection();
     }
+
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
