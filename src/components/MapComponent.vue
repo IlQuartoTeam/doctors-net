abbb88a28967fdbc311319c4a0155ea148eccdb6
filecc <fil.gia@live.it>
2023-07-03 17:44:09 +0200

    doctor component modifed and remove

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -65,7 +65,7 @@ export default {
         })
         .catch((err) => 
         {
-          
+          console.log(err);
         })
         
     },
@@ -74,6 +74,7 @@ export default {
         this.map.off()
         this.map.remove()
         this.initializeMap(value)
+        this.putPinsOnMap()
       }
     },
     putPinsOnMap()
@@ -93,14 +94,17 @@ export default {
       if(this.doctors)
       {
         this.doctors.forEach(element => {
+         element.city != 'Roma' && console.log(element);
           const marker = L.marker([element.address_lat, element.address_long], {icon: icon}).addTo(this.map);
+          const specialization = element.specializations[0].name ?? 'Medicina Generale'
           const popup = `
             <h6 class="markerPopup-name text-center">${element.name} ${element.surname}</h6>
-            <p class="text-center m-0 p-0 mb-2">${element.specializations[0].name ?? 'Medicina Generale'}</p>
+            <p class="text-center m-0 p-0 mb-2">${specialization}</p>
             <a class="d-block text-center text-doc-primary text-underline popup-link" href="/doctors/${element.slug}">Dettagli</a>
             `
           marker.bindPopup(popup)
           })
+         
       }
       
     }
