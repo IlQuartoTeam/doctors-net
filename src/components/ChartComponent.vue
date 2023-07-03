82b92e06125352366d05ef151eda7b88b2b4fa20
nicolozibra1 <nico.zibra@gmail.com>
2023-07-03 21:25:56 +0200

    chart layout structure

--- src/components/ChartComponent.vue
+++ src/components/ChartComponent.vue
@@ -1,6 +1,15 @@
 <template>
-    <div v-if="loaded" class="chart-container">
+  <h1 class="text-h2 text-doc-blue fw-semibold text-center mt-4">La tua dashboard</h1>
+    <div v-if="loaded" class="row">
+      <div class="col-12 col-lg-6 px-5 pt-4">
+        <div class="timeframe d-flex justify-content-end me-4 gap-1">
+          <span class="badge bg-primary fw-light">1 Y</span>
+          <span class="badge bg-primary fw-light">1 M</span>
+          <span class="badge bg-primary fw-light">1 W</span>
+          <span class="badge bg-primary fw-light selected">1 D</span>
+        </div>
         <Line :data="data" :options="options" />
+      </div>
     </div>
   </template>
   
@@ -40,7 +49,12 @@
         loaded: false,
         data: {
           labels: [],
-          datasets: [{ data: [40, 80, 60, 120, 10, 30] }]
+          datasets: [
+            { data: [40, 80, 60, 120, 10, 30],
+              label: 'Messaggi Ricevuti',
+              backgroundColor: '#F38F23',
+            }
+          ]
         },
         options: {
             responsive: true,
@@ -64,3 +78,12 @@
     }
   }
   </script>
+
+<style>
+  .selected{
+    background-color: #E26439 !important;
+    color: black;
+    font-weight: bold !important;
+    box-shadow: rgba(50, 50, 93, 0.062) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.123) 0px 18px 36px -18px inset;
+  }
+</style>
