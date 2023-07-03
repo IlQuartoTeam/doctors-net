4bf348218588847a006ec7e7f4f5fad68bdfbc03
filecc <fil.gia@live.it>
2023-07-03 10:23:45 +0200

    some fix

--- src/Views/Login.vue
+++ src/Views/Login.vue
@@ -110,7 +110,6 @@ export default {
                     store.userDoctor = { ...res.data.doctor, ...res.data.user }
                     console.log(store.userDoctor)
                     router.push('/users/profile')
-
                 }).catch(err => {
                     this.message.text = 'Ooops! Si è verificato un errore.'
                     this.loading = false
