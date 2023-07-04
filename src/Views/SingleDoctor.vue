200b7ce8f3d679ebc8536b9d6686a6a014c64e12
nicolozibra1 <nico.zibra@gmail.com>
2023-07-04 17:25:26 +0200

    getDoctor called

--- src/Views/SingleDoctor.vue
+++ src/Views/SingleDoctor.vue
@@ -1,20 +1,34 @@
 <template>
     <div>
-        {{ this.$route.params }}
        <ExperiencesComponent />
     </div>
 </template>
 
 <script>
+import { store } from '../store/store';
+import axios from 'axios';
 import ExperiencesComponent from '../components/ExperiencesComponent.vue';
     export default {
         components: { ExperiencesComponent},
         data () {
-
-        return {
-           
+            return {
+                store,
+            }
+        },
+        methods: {
+            getDoctor() {
+                axios.get(store.API_URL + 'doctors/' + this.$route.params.user).then(res => {
+                    store.singleDoctor = res.data.results;
+                }).catch(err => {
+                    this.message.text = 'Ooops! Si è verificato un errore.'
+                    this.loading = false
+                });
+            }
+            
+        },
+        mounted() {
+            this.getDoctor()
         }
-    }
     }
 </script>
 
