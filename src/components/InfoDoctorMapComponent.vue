9db6f139cb4612330b62b8ce5494aca55fa61e8e
Gaetano26 <gaetano.spampinato@outlook.it>
2023-07-05 16:59:30 +0200

    fixed address

--- src/components/InfoDoctorMapComponent.vue
+++ src/components/InfoDoctorMapComponent.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col info">
-                <p class="d-flex align-items-center"><IconMapPin :color="$doc-primary"/><span class="ps-2">{{ store.singleDoctor.address }}</span><span class="ps-2">{{ store.singleDoctor.city }}</span></p>
+                <p class="d-flex align-items-center"><IconMapPin :color="$doc-primary"/><span v-for="address in takeAddress" class="ps-2">{{ address }}</span><span v-if="index !== takeAddress.length - 1">,</span><span class="ps-2">{{ store.singleDoctor.city }}</span></p>
                 <p class="d-flex align-items-center gap-2"><IconPhone :color="$doc-primary" /><span>{{ store.singleDoctor.phone }}</span></p>
                 <p class="d-flex align-items-center gap-2"><IconMail :color="$doc-primary" /><span>{{ store.singleDoctor.email }}</span></p>
             </div>
@@ -22,6 +22,16 @@ import { store } from '../store/store';
 import { IconMail, IconPhone, IconMapPin } from '@tabler/icons-vue';
 export default {
     components: { IconMail, IconPhone, IconMapPin},
+    computed: {
+        takeAddress() {
+      if (this.store.singleDoctor.address) {
+        return this.store.singleDoctor.address.split(",");
+      } else {
+        return []
+      }
+      }
+ 
+    },
     data () {
         
 
