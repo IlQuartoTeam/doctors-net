08db04de12bac02c512bce3847dc88d563450623
Gaetano26 <gaetano.spampinato@outlook.it>
2023-07-05 21:40:40 +0200

    fixed ExperiencesComponent

--- src/components/ExperiencesComponent.vue
+++ src/components/ExperiencesComponent.vue
@@ -1,38 +1,76 @@
 <template>
-    <div> 
-        <div class="container m-0">
-            <div class="row">
-                <div class="col">
-                  <ul class="py-4">
-                        <h2>Esperienze</h2>
-                        <li  v-for="(experience, index) in data.experiences">{{ experience.name }}</li>
-                    </ul>
-                    <ul class="py-4">
-                        <h2>Formazione</h2>
-                        <li v-for="(formation, index) in data.formations">{{ formation.name }}</li>
-                    </ul>
+  <div>
+    <div class="container m-0">
+      <div class="row">
+        <div class="col">
+          <div class="exp">
+            <h2>Esperienze Lavorative</h2>
+            <ul class="p-0">
+              <li v-for="experience in filteredExperiences('work')" :key="experience.id">
+                <div class="d-flex flex-column">
+                  <span>{{ experience.name }}</span>
+                 <div class="d-flex gap-4">
+                  <p class="dark">Data inizio: {{ experience.start_date }}</p>
+                  <p class="dark">Data fine: <span v-if="experience.end_date">{{ experience.end_date }}</span><span v-else>In corso</span></p>
                 </div>
-                
-            </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="exp">
+            <h2>Esperienze Lavorative</h2>
+            <ul class="p-0">
+              <li v-for="experience in filteredExperiences('education')" :key="experience.id">
+                <div class="d-flex flex-column">
+                  <span>{{ experience.name }}</span>
+                 <div class="d-flex gap-4">
+                  <p class="dark">Data inizio: {{ experience.start_date }}</p>
+                  <p class="dark">Data fine: <span v-if="experience.end_date">{{ experience.end_date }}</span><span v-else>In corso</span></p>
+                </div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
+
 <script>
+import { store } from '../store/store';
 export default {
     data () {
         
 
         return {
-            data: {}
+            store
         }
+    },
+    methods: {
+    filteredExperiences(type) {
+      return this.store.singleDoctor.experiences.filter(experience => experience.type === type);
     }
+  }
 }
+
 </script>
 
 <style lang="scss" scoped>
+
 @use "../assets/styles/_variables.scss" as *;
 
+.dark {
+  color: black;
+}
+.exp {
+  padding-left: 15px;
+}
+p {
+  color: rgba($doc-dark, 0.6);
+  font-weight: bold;
+}
 
 h2 {
     color: $doc-blue;
@@ -46,7 +84,7 @@ ul {
 
 li {
   position: relative; 
-  padding-left: 27px; 
+  padding-left: 25px; 
   color: rgba($doc-dark, 0.6);
   font-weight: bold;
 }
@@ -54,8 +92,8 @@ li {
 li:before {
   content: "";
   position: absolute; 
-  left: 3.5%; 
-  top: 50%; 
+  left: 3%; 
+  top: 18%; 
   transform: translate(-50%, -50%); 
   width: 8px; 
   height: 8px; 
@@ -69,18 +107,21 @@ li:before {
 
 
 @media only screen and (min-width: 768px) {
-
+   
+  .exp {
+  padding-left: 20px;
+}
 
     li {
   position: relative; 
-  padding-left: 27px; 
+  padding-left: 20px; 
 }
 
 li:before {
   content: "";
   position: absolute; 
-  left: 2.5%; 
-  top: 50%; 
+  left: 1.6%; 
+  top: 18%; 
   transform: translate(-50%, -50%); 
   width: 8px; 
   height: 8px; 
@@ -118,8 +159,8 @@ h2 {
 li:before {
   content: "";
   position: absolute; 
-  left: 1.7%; 
-  top: 50%; 
+  left: 3%; 
+  top: 20%; 
   transform: translate(-50%, -50%); 
   width: 10px; 
   height: 10px; 
@@ -158,8 +199,8 @@ li:before {
 li:before {
   content: "";
   position: absolute; 
-  left: 1.3%; 
-  top: 50%; 
+  left: 2.3%; 
+  top: 20%; 
   transform: translate(-50%, -50%); 
   width: 10px; 
   height: 10px; 
