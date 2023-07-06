6021b2441c1b76d600419bc3552c121ae5a9a21e
Lip-7 <zhighofficial@gmail.com>
2023-07-06 18:21:02 +0200

    minor fixes

--- src/components/HeroUserComponent.vue
+++ src/components/HeroUserComponent.vue
@@ -3,21 +3,27 @@
         <h1 class="text-h1 fw-bold text-white mt-5">Ciao, {{ store.userDoctor.name }} {{ store.userDoctor.surname }}</h1>
         <span class="text-white text-decoration-underline mb-5">Hai {{ store.userDoctor.messages.length }} messaggi non letti.</span>
     </div>
+    <div class="wrap p-5">
+        <StarsComponent/>
+    </div>
 </template>
 
 <script>
+import StarsComponent from './StarsComponent.vue';
+
 import { store } from '../store/store';
     export default {
-        name:'HeroUserComponent',
-        props: ['doctor'],
-        data(){
-            return{
-                store,
-            }
-        },
-        mounted() {
-        }
-    }
+    name: "HeroUserComponent",
+    props: ["doctor"],
+    data() {
+        return {
+            store,
+        };
+    },
+    mounted() {
+    },
+    components: { StarsComponent }
+}
     
 </script>
 
