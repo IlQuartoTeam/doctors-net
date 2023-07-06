523943a32167d74c76eada5ae5621a5429e0fc6c
Lip-7 <zhighofficial@gmail.com>
2023-07-06 17:05:50 +0200

    minor fixes on reviews

--- src/Views/User.vue
+++ src/Views/User.vue
@@ -2,105 +2,109 @@
     <!-- {{ $route.params.id }}
     {{ store.user }}
     {{ store.doctor }} -->
-     <div class="box-component d-flex w-100 overflow-hidden overflow-sm-auto">
-         <div class="sidebar">
-             <SidebarComponent />
-         </div>
-         <div class="content position-relative flex-grow-1">
-             <HeroUserComponent v-if="store.dashboard.heroOpen" />
-             <!-- <ChartComponent v-if="store.dashboard.chartsOpen" /> -->
-             <MessageUserComponent v-if="store.dashboard.messaggesOpen" />
-             <SettingUserComponent v-if="store.dashboard.generalInfo" />
-             <ExperiencesUserComponent v-if="store.dashboard.experiences"  />
-             <PerformancesUserComponent v-if="store.dashboard.performances" />
-             <ChangeUserPassword v-if="store.dashboard.changePassword" />
-             <div class="sideButton d-block d-lg-none py-2">
-                 <div v-if="!store.dashboard.sidebarOpen" @click="() => store.dashboard.sidebarOpen = !store.dashboard.sidebarOpen" class="open-side d-flex align-items-center" :class="[store.dashboard.messaggesOpen === true ? 'bg-variable' : '']">
-                     <IconChevronRight class="ms-3" />
-                 </div>
-             </div>
-         </div>
-     </div>
-     
-    
-    
-    
- </template>
+    <div class="box-component d-flex w-100 overflow-hidden overflow-sm-auto">
+        <div class="sidebar">
+            <SidebarComponent />
+        </div>
+        <div class="content position-relative flex-grow-1">
+            <HeroUserComponent v-if="store.dashboard.heroOpen" />
+            <!-- <ChartComponent v-if="store.dashboard.chartsOpen" /> -->
+            <MessageUserComponent v-if="store.dashboard.messaggesOpen" />
+            <SettingUserComponent v-if="store.dashboard.generalInfo" />
+            <ExperiencesUserComponent v-if="store.dashboard.experiences" />
+            <ProfileReviews v-if="store.dashboard.reviewsOpen"/>
+            <PerformancesUserComponent v-if="store.dashboard.performances" />
+            <ChangeUserPassword v-if="store.dashboard.changePassword" />
+            <div class="sideButton d-block d-lg-none py-2">
+                <div v-if="!store.dashboard.sidebarOpen"
+                    @click="() => store.dashboard.sidebarOpen = !store.dashboard.sidebarOpen"
+                    class="open-side d-flex align-items-center"
+                    :class="[store.dashboard.messaggesOpen === true ? 'bg-variable' : '']">
+                    <IconChevronRight class="ms-3" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
  
- <script>
- import DoctorCard from '../components/DoctorCard.vue';
- import axios from 'axios';
- import router from '../router/router';
- import { store } from '../store/store';
- import { IconHome } from '@tabler/icons-vue';
- import { IconChevronRight } from '@tabler/icons-vue';
- import HeroUserComponent from '../components/HeroUserComponent.vue';
- import ChartComponent from '../components/ChartComponent.vue';
- import SidebarComponent from '../components/SidebarComponent.vue';
- import MessageUserComponent from '../components/MessageUserComponent.vue';
- import SettingUserComponent from '../components/SettingUserComponent.vue';
- import PerformancesUserComponent from '../components/PerformancesUserComponent.vue';
- import ChangeUserPassword from '../components/ChangeUserPassword.vue';
- import ExperiencesUserComponent from '../components/ExperiencesUserComponent.vue';
+<script>
+import ProfileReviews from '../components/ProfileReviews.vue';
+import DoctorCard from '../components/DoctorCard.vue';
+import axios from 'axios';
+import router from '../router/router';
+import { store } from '../store/store';
+import { IconHome } from '@tabler/icons-vue';
+import { IconChevronRight } from '@tabler/icons-vue';
+import HeroUserComponent from '../components/HeroUserComponent.vue';
+import ChartComponent from '../components/ChartComponent.vue';
+import SidebarComponent from '../components/SidebarComponent.vue';
+import MessageUserComponent from '../components/MessageUserComponent.vue';
+import SettingUserComponent from '../components/SettingUserComponent.vue';
+import PerformancesUserComponent from '../components/PerformancesUserComponent.vue';
+import ChangeUserPassword from '../components/ChangeUserPassword.vue';
+import ExperiencesUserComponent from '../components/ExperiencesUserComponent.vue';
+
+export default {
+    components: { DoctorCard, HeroUserComponent, SidebarComponent, MessageUserComponent, SettingUserComponent, PerformancesUserComponent, ChartComponent, ChangeUserPassword, IconChevronRight, ExperiencesUserComponent, ProfileReviews },
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        getUser() {
+            if (this.$cookies.get("session-token")) {
+                const token = this.$cookies.get("session-token")
+
+                const config = { headers: { Authorization: `Bearer ${token}` } }
+                axios.post(store.API_URL + 'user', { key: 'value' }, config).then(res => {
+                    store.doctor = res.data.doctor
+                    store.user = res.data.user
+                    store.userDoctor = { ...res.data.doctor, ...res.data.user }
+                    router.push('/users/profile')
+
+                }).catch(err => {
+                    this.message.text = 'Ooops! Si è verificato un errore.'
+                    this.loading = false
+                })
+            }
+        },
+    },
+    mounted() {
+        if (!this.$cookies.get("session-token") && !store.user) {
+            router.push('/login')
+        } else {
+            this.getUser()
+        }
+    }
+}
+</script>
  
-     export default {
-         components: { DoctorCard, HeroUserComponent, SidebarComponent, MessageUserComponent, SettingUserComponent, PerformancesUserComponent, ChartComponent, ChangeUserPassword, IconChevronRight, ExperiencesUserComponent },
-         data(){
-             return{
-                 store,
-             }
-         },
-         methods: {
-             getUser(){
-                 if ( this.$cookies.get("session-token")){
-                     const token = this.$cookies.get("session-token")
-                
-                     const config = { headers: { Authorization: `Bearer ${token}` }}
-                     axios.post(store.API_URL + 'user', {key: 'value'}, config).then(res => {
-                             store.doctor = res.data.doctor
-                             store.user = res.data.user
-                             store.userDoctor = {...res.data.doctor, ...res.data.user}
-                             router.push('/users/profile')
- 
-                         }).catch(err => {
-                             this.message.text = 'Ooops! Si è verificato un errore.'
-                             this.loading = false
-                         })
-             }
-         },
-     },
-     mounted(){
-         if (!this.$cookies.get("session-token") && !store.user){
-             router.push('/login')
-         } else {
-             this.getUser()
-         }
-     }
- }
- </script>
- 
- <style lang="scss" scoped>
- .sideButton{
-     position: fixed;
-     top: 80%;
-     left: 0px;
-     transform: translateY(-50%);
- }
- .open-side{
-         background-color: rgb(255, 255, 255);
-         box-shadow: rgba(0.24, 0.24, 0, 0.24) 0px 3px 6px;
-         width: 50px;
-         height: 50px;
-         margin-left: -10px;
-         border-radius: 5px;
-         transition: margin-left 0.3s;
-         &:hover{
-             cursor: pointer;
-             margin-left: -5px;
-             background-color: rgba(255, 255, 255, 0.548);
-         }
-     }
-     @media screen and (min-width:1200px){
-       /*   */
-     }
- </style>
+<style lang="scss" scoped>
+.sideButton {
+    position: fixed;
+    top: 80%;
+    left: 0px;
+    transform: translateY(-50%);
+}
+
+.open-side {
+    background-color: rgb(255, 255, 255);
+    box-shadow: rgba(0.24, 0.24, 0, 0.24) 0px 3px 6px;
+    width: 50px;
+    height: 50px;
+    margin-left: -10px;
+    border-radius: 5px;
+    transition: margin-left 0.3s;
+
+    &:hover {
+        cursor: pointer;
+        margin-left: -5px;
+        background-color: rgba(255, 255, 255, 0.548);
+    }
+}
+
+@media screen and (min-width:1200px) {
+    /*   */
+}
+</style>
---
+++ src/components/ProfileReviews.vue
@@ -0,0 +1,65 @@
+<template>
+    <div v-if="store.userDoctor?.reviews.length > 0" class="reviewsWrapper">
+        <div class="container-fluid mt-5 px-5">
+            <div class="box-reviews container-fluid">
+                <div class="row mb-5 pt-3" v-for="review in store.userDoctor.reviews">
+                    <div class="col-12">
+                        <h2 class="fw-semibold name">{{ review.name }}</h2>
+                        <div class="date-review d-flex align-items-center gap-5">
+                            <span>{{ review.created_at }} </span>
+                            <div class="d-flex align-items-center justify-content-center pb-2">
+                                <span v-for="(star, index) in review.rating">
+                                    <IconStarFilled class="text-doc-accent" v-if="star" />
+                                </span>
+                                <span v-for="star in (5 - review.rating)">
+                                    <IconStar />
+                                </span>
+                            </div>
+                        </div>
+                        <p class="mt-2">{{ review.text }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-if="store.userDoctor?.reviews.length < 1" class="noReviews text-center pt-3 row justify-content-center">
+        <div class="medikit col-6">
+            <img class="img-fluid" src="/img/other/medikit.png" alt="">
+        </div>
+        <h2 class="col-10">Ma non te se ncula popo nessuno eh?!</h2>
+        <!-- <h2>Non hai ancora nessun messaggio</h2> -->
+    </div>
+</template>
+
+<script>
+import { IconStar } from '@tabler/icons-vue';
+import { IconStarFilled } from '@tabler/icons-vue';
+import { store } from '../store/store';
+export default {
+    name: "ProfileReviews",
+    data() {
+        return {
+            store,
+        };
+    },
+    mounted() {
+        console.log(store.userDoctor);
+    },
+    methods: {},
+    components: { IconStarFilled, IconStar }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/styles/variables' as *;
+.medikit {
+    max-width: 350px;
+    min-width: 200px;
+}
+.name {
+    font-size: 24px;
+    font-weight: bold;
+    color: $doc-blue;
+    letter-spacing: 1px;
+}
+</style>
