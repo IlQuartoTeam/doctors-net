cf05ac7046f0e801e6dd11fad3cad1e464869848
Lip-7 <zhighofficial@gmail.com>
2023-07-06 18:04:01 +0200

    minor fixes on messages user

--- src/components/MessageUserComponent.vue
+++ src/components/MessageUserComponent.vue
@@ -1,16 +1,16 @@
 <template>
-    <div v-if="store.userDoctor.messages.length > 0" class="container-fluid" id="index">
+    <div v-if="store.userDoctor.messages.length > 0 && !isOpenMessage" class="container-fluid" id="index">
         <h1 class="text-h2 text-doc-blue fw-semibold text-center">I tuoi messaggi</h1>
         <table class="table">
             <thead>
-                <tr>
+                <tr >
                     <th scope="col" class="w-25 d-none">Nome</th>
                     <th scope="col" class="w-25">Email</th>
                     <th scope="col" class="w-50">Messaggio</th>
                 </tr>
             </thead>
             <tbody v-for="(message, index) in store.userDoctor.messages">
-                <tr @click="openMessage" class="prev-message">
+                <tr @click="openMessage(index)" class="prev-message">
                     <td class="d-none">{{ message.fullname }}</td>
                     <td>{{ message.email }}</td>
                     <!-- <td class="d-none">{{ truncateMessage(message.text, 30) }}</td> -->
@@ -20,18 +20,19 @@
                 </tr>
             </tbody>
         </table>
-        <div class="text-center mt-4">
-            <ButtonComponent @click="toggledashboardActive" className="button-doctor outline">Torna alla Dashboard
-            </ButtonComponent>
-        </div>
     </div>
     <div v-if="isOpenMessage" class="container-fluid mt-4" id="show">
         <h1 class="text-h2 text-doc-blue fw-semibold text-center mb-4">Messaggio</h1>
-        <div v-for="(message, index) in store.userDoctor.messages" class="message-details d-flex flex-column gap-2">
+        <div class="message-details d-flex flex-column gap-2">
+            <h6><span class="fw-semibold">Ricevuto da: </span>{{ messageToView.fullname }}</h6>
+            <h6><span class="fw-semibold">Email: </span>{{ messageToView.email }}</h6>
+            <h6><span class="fw-semibold">Messaggio: </span>{{ messageToView.text }}</h6>
+        </div>
+        <!-- <div v-for="(message, index) in store.userDoctor.messages" class="message-details d-flex flex-column gap-2">
             <h6><span class="fw-semibold">Ricevuto da: </span>{{ message.fullname }}</h6>
             <h6><span class="fw-semibold">Email: </span>{{ message.email }}</h6>
             <h6><span class="fw-semibold">Messaggio: </span>{{ message.text }}</h6>
-        </div>
+        </div> -->
         <div class="text-center mt-4">
             <ButtonComponent @click="openMessage" className="button-doctor outline">Indietro</ButtonComponent>
         </div>
@@ -68,8 +69,9 @@ export default {
                 return message.slice(0, length) + "...";
             }
         },
-        openMessage() {
-            this.isOpenMessage = !this.isOpenMessage
+        openMessage(index) {
+            this.isOpenMessage = !this.isOpenMessage;
+            this.messageToView = store.userDoctor.messages[index];
         },
         toggledashboardActive() {
             store.dashboard.heroOpen = !store.dashboard.heroOpen;
