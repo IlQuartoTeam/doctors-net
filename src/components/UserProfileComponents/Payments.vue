a06b0f90f812f4fcabe4554f4b9a7bf06b5bb0eb
filecc <fil.gia@live.it>
2023-07-11 09:07:18 +0200

    button in payment modal

--- src/components/UserProfileComponents/Payments.vue
+++ src/components/UserProfileComponents/Payments.vue
@@ -1,31 +1,33 @@
 <template>
-     <div class="modal-wrap">
-        <div class="doc-modal p-4">
-          <h1 class="text-center text-doc-blue fs-2 fw-bold">Stai acquistando</h1>
-          <p class="ps-2 text-center text-doc-blue fs-4 fw-bold">{{ store.payment.subscription + '   (' +store.payment.hours+ ')' + '  -   '+ store.payment.price }}€</p>
+  <div class="modal-wrap">
+    <div class="doc-modal p-4">
+      <h1 class="text-center text-doc-blue fs-2">Stai acquistando</h1>
+      <p class="ps-2 text-center text-doc-blue fs-4 fw-bold">{{ store.payment.subscription + ' (' + store.payment.hours +
+        ')' + ' - ' + store.payment.price }}€</p>
 
 
-            <div class="content mt-1 container-fluid">
-              <div id="dropin-container"></div>
-                 <form id="payment-form" action="" method="post">
-      <!-- Putting the empty container you plan to pass to
+      <div class="content mt-1 container-fluid">
+        <div id="dropin-container"></div>
+        <form id="payment-form" action="" method="post">
+          <!-- Putting the empty container you plan to pass to
       'braintree.dropin.create' inside a form will make layout and flow
       easier to manage -->
-      <ButtonComponent id="payment-button" :button="true" type="submit" className="primary">Aggiungi carta</ButtonComponent>
-      <ButtonComponent  @click="store.payMode = false" :button="false" className="btn outline  ms-3">Annulla</ButtonComponent>
+      <div class="d-flex justify-content-center align-items-center gap-2">
+        <ButtonComponent :disabled="loading" id="payment-button" :button="true" type="submit" className="primary flex-grow-1">
+         aggiungi carta
+          </ButtonComponent>
+          <ButtonComponent @click="store.payMode = false" :button="false" className="outline-accent flex-grow-1">Annulla
+          </ButtonComponent>
+      </div>
+        
 
-
-
-
-
-    </form>
-            </div>
-        </div>
+        </form>
+      </div>
     </div>
-  <div>
-   
   </div>
+  <div>
 
+  </div>
 </template>
 
 <script>
@@ -34,13 +36,15 @@ import { store } from '../../store/store'
 import ButtonComponent from '../ButtonComponent.vue';
 
 
+
 export default {
   data() {
     return {
       token: '',
-      nonce:'',
+      nonce: '',
       store,
-      paymentAdded: false
+      paymentAdded: false,
+      loading: false
 
     }
   },
@@ -69,14 +73,14 @@ export default {
             }
             else {
 
-            // Step four: when the user is ready to complete their
-            //   transaction, use the dropinInstance to get a payment
-            //   method nonce for the user's selected payment method, then add
-            //   it a the hidden field before submitting the complete form to
-            //   a server-side integration
-            this.nonce = payload.nonce;
-            this.paymentAdded = true;
-            document.querySelector('#payment-button').textContent = "Paga ora"
+              // Step four: when the user is ready to complete their
+              //   transaction, use the dropinInstance to get a payment
+              //   method nonce for the user's selected payment method, then add
+              //   it a the hidden field before submitting the complete form to
+              //   a server-side integration
+              this.nonce = payload.nonce;
+              this.paymentAdded = true;
+              document.querySelector('#payment-button').textContent = "Paga ora"
 
             }
 
@@ -86,52 +90,54 @@ export default {
     },
     getToken() {
       axios.get('api/payment/initialize/')
-            .then((res) => {
-                this.token = res.data.token
-                this.initializePayment(this.token)
+        .then((res) => {
+          this.token = res.data.token
+          this.initializePayment(this.token)
 
 
-            }).catch((err) => {
+        }).catch((err) => {
 
 
-            })
+        })
     },
     pay() {
+    
       const paymentData = {
-      amount: this.store.payment.price, 
-      payment_method_nonce: this.nonce, 
-      subID: this.store.payment.subID
-    };
-    const config =
-            {
-                headers: { Authorization: `Bearer ${this.$cookies.get('session-token')}` }
-            }
+        amount: this.store.payment.price,
+        payment_method_nonce: this.nonce,
+        subID: this.store.payment.subID
+      };
+      const config =
+      {
+        headers: { Authorization: `Bearer ${this.$cookies.get('session-token')}` }
+      }
       axios.post('/api/payment/process', paymentData, config)
-      .then(response => {
-        // Elaborazione della risposta dal backend
-        const success = response.data.success;
-        if (success) {
-          store.toast.success('Sottoscrizione acquistata!', { timeout: 1500 });
-          this.store.payMode = false;
+        .then(response => {
+          // Elaborazione della risposta dal backend
+          const success = response.data.success;
+          if (success) {
+            store.toast.success('Sottoscrizione acquistata!', { timeout: 1500 });
+            this.store.payMode = false;
+            
 
-        } else {
-          store.toast.error('Pagamento non riuscito', { timeout: 1500 });
-          const error = response.data.error;
-          this.store.payMode = false;
+          } else {
+            store.toast.error('Pagamento non riuscito', { timeout: 1500 });
+            const error = response.data.error;
+            this.store.payMode = false;
+            console.error(error);
+          }
+        })
+        .catch(error => {
+          // Gestione degli errori di comunicazione con il backend
           console.error(error);
-        }
-      })
-      .catch(error => {
-        // Gestione degli errori di comunicazione con il backend
-        console.error(error);
-      });
+        });
     }
   },
   components: {
-        ButtonComponent,
-      
-    },
-      mounted() {
+    ButtonComponent,
+
+  },
+  mounted() {
     this.getToken()
 
 
@@ -140,34 +146,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 .modal-wrap {
-    position: fixed;
-    width: 100%;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    display: grid;
-    place-items: center;
-    z-index: 999999;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  display: grid;
+  place-items: center;
+  z-index: 999999;
 }
 
 .doc-modal {
-    background-color: white;
-    width: 500px;
-    position: relative;
-    border-radius: 10px;
+  background-color: white;
+  width: 500px;
+  position: relative;
+  border-radius: 10px;
 
-    @media screen and (max-width: 768px) {
-        width: 100%;
-        height: 100%;
-    }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
 }
 
 
 [data-braintree-id="toggle"] {
   display: none;
 }
-
 </style>
